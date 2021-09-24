6abf5c6b2ced5d8e1c7315b0fff53f5d1c013562
Zoya Tanweer <87615284+zoyatanweer@users.noreply.github.com>
2021-09-24 14:40:41 +0530

    cash-register-manager
    A cash register app which gives the number of notes to be returned on the basis of bill amount and cash provided.

---
+++ README.md
@@ -0,0 +1,5 @@
+**Cash Register**
+
+A cash register which gives the number of notes to be returned based on the bill amount and cash given. 
+
+I used HTML, CSS and JavaScript to make this.
---
+++ app.js
@@ -0,0 +1,51 @@
+const billAmount = document.querySelector('#bill-amount');
+const cashGiven = document.querySelector('#cash-given');
+const checkButton = document.querySelector('#check-button');
+const message = document.querySelector('#message');
+const noOfNotes = document.querySelectorAll('.no-of-notes');
+
+//available notes we have
+const availableNotes = [2000,500,100,20,10,5,1]
+
+
+checkButton.addEventListener("click", function validateBillAndCashAmount(){
+    hideMessage();
+    if (billAmount !==""){
+     if( Number(billAmount.value>0) ){
+            if ( (Number(cashGiven.value)) >= (Number (billAmount.value)) ) {
+                // console.log(billAmount.value) 
+                // console.log(cashGiven.value);
+                const amountReturn = cashGiven.value - billAmount.value;
+                // console.log(amountReturn);
+                calculateChange(amountReturn)
+            }
+            else {
+                showMessage("The cash provided should atleast be equal to the bill amount.")
+            }
+    }
+    else {
+        showMessage("Please enter the values.");
+    }
+}
+
+})
+
+function calculateChange(amountReturn){
+    for(let i = 0; i<availableNotes.length ; i++){
+        const numberOfNotes =  Math.trunc( 
+            amountReturn / availableNotes[i]
+        );
+    
+    amountReturn = amountReturn%availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
+function hideMessage(){
+    message.style.display = "none";
+}
+
+function showMessage(msg){
+    message.style.display="block";
+    message.innerText = msg;
+}
